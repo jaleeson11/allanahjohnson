641bbc4c6c66acf0c6f3c94a8cf6d50b45c03f85
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2023-05-19 15:30:56 +0100

    Removed default sidebar widget

--- functions.php
+++ functions.php
@@ -119,20 +119,20 @@ add_action( 'after_setup_theme', 'allanahjohnson_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function allanahjohnson_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'allanahjohnson' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'allanahjohnson' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'allanahjohnson_widgets_init' );
+// function allanahjohnson_widgets_init() {
+// 	register_sidebar(
+// 		array(
+// 			'name'          => esc_html__( 'Sidebar', 'allanahjohnson' ),
+// 			'id'            => 'sidebar-1',
+// 			'description'   => esc_html__( 'Add widgets here.', 'allanahjohnson' ),
+// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 			'after_widget'  => '</section>',
+// 			'before_title'  => '<h2 class="widget-title">',
+// 			'after_title'   => '</h2>',
+// 		)
+// 	);
+// }
+// add_action( 'widgets_init', 'allanahjohnson_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
