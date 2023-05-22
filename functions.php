3af7655e9ad5bcdd16fd6d9559816ab0524ddf12
jaleeson11 <66011869+jaleeson11@users.noreply.github.com>
2023-05-22 18:05:18 +0100

    Registered footer widgets

--- functions.php
+++ functions.php
@@ -119,20 +119,31 @@ add_action( 'after_setup_theme', 'allanahjohnson_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-// function allanahjohnson_widgets_init() {
-// 	register_sidebar(
-// 		array(
-// 			'name'          => esc_html__( 'Sidebar', 'allanahjohnson' ),
-// 			'id'            => 'sidebar-1',
-// 			'description'   => esc_html__( 'Add widgets here.', 'allanahjohnson' ),
-// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-// 			'after_widget'  => '</section>',
-// 			'before_title'  => '<h2 class="widget-title">',
-// 			'after_title'   => '</h2>',
-// 		)
-// 	);
-// }
-// add_action( 'widgets_init', 'allanahjohnson_widgets_init' );
+function allanahjohnson_widgets_init() {
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Column 1', 'allanahjohnson' ),
+			'id'            => 'sidebar-footer-1',
+			'description'   => esc_html__( 'Add widgets here.', 'allanahjohnson' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		),
+	);
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Column 2', 'allanahjohnson' ),
+			'id'            => 'sidebar-footer-2',
+			'description'   => esc_html__( 'Add widgets here.', 'allanahjohnson' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		),
+	);
+}
+add_action( 'widgets_init', 'allanahjohnson_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
